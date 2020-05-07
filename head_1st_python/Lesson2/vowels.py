57b497a3852d735c21a50a6f9b8f9d2d638dd387
Vamshi Majhi <codeblurred@gmail.com>
2020-05-07 12:41:23 +0530

    Lesson2 commit part 2 - series1

--- head_1st_python/Lesson2/vowels.py
+++ head_1st_python/Lesson2/vowels.py
@@ -1,7 +1,15 @@
 vowels = ['a', 'e', 'i', 'o', 'u']
 
-word = "Milliways"
+word = input("provide a word to search for vowels : ")
+
+
+found = [] # starts with empty list
 
 for letter in word:    # takes each letter in the word
     if letter in vowels:   # and if it matches in the vowels list
-        print (letter)     # display the letter
+        if letter not in found:
+            found.append(letter)
+
+
+for vowel in found:
+    print(vowel)
